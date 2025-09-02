32bf3be8ceccf96046366b9326b271b3a8a6f757
Kacper Turek <115101474+kTurek95@users.noreply.github.com>
2025-09-02 23:23:32 +0200

    add company logo with external link on Login and Register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,17 @@ import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <div className="d-flex justify-content-center align-items-center min-vh-100 bg-light">
-      <div className="card p-4 shadow" style={{ width: "350px" }}>
-        <h2 className="text-center mb-4">🔐 Logowanie</h2>
+      <div className="card p-4 main-container">
+        <a className="text-center mb-3" href="https://inqse.com/" target="_blank">
+          <img
+            src="https://quguse.pl/img/INQSE_logo.png"
+            alt="Logo INQSE"
+            className="logo"
+          />
+        </a>
+        <h3 className="text-center">🔐 Logowanie</h3>
         <Form route="/api/token/" method="login" />
-        <p className="text-center mt-3 mb-0">
+        <p className="text-center mt-0 mb-0">
           Nie masz konta? <Link to="/register">Zarejestruj się</Link>
         </p>
       </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,7 +5,14 @@ export default function Register() {
   return (
     <div className="d-flex justify-content-center align-items-center min-vh-100 bg-light">
       <div className="card p-4 shadow" style={{ width: "350px" }}>
-        <h2 className="text-center mb-4">📝 Rejestracja</h2>
+        <a className="text-center mb-3" href="https://inqse.com/" target="_blank">
+          <img
+            src="https://quguse.pl/img/INQSE_logo.png"
+            alt="Logo INQSE"
+            className="logo"
+          />
+        </a>
+        <h2 className="text-center">📝 Rejestracja</h2>
         <Form route="/api/user/register/" method="register" />
         <p className="text-center mt-3 mb-0">
           Masz już konto? <Link to="/login">Zaloguj się</Link>
